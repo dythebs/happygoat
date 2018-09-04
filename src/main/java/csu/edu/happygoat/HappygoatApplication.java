d072536043b5c6560b0e3f90e5739039abf450ac
dythebs <244271172@qq.com>
2018-09-04 09:54:14 +0800

    first commit

---
+++ src/main/java/csu/edu/happygoat/HappygoatApplication.java
@@ -0,0 +1,12 @@
+package csu.edu.happygoat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappygoatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappygoatApplication.class, args);
+    }
+}
---
+++ src/test/java/csu/edu/happygoat/HappygoatApplicationTests.java
@@ -0,0 +1,16 @@
+package csu.edu.happygoat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HappygoatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
