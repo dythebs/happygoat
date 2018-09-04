4cfec6cf4457951fc1e3165cb468caf77f049ef1
dythebs <244271172@qq.com>
2018-09-04 13:26:06 +0800

    搭建基本的工程框架

--- src/main/java/csu/edu/happygoat/HappygoatApplication.java
+++ src/main/java/csu/edu/happygoat/HappygoatApplication.java
@@ -1,9 +1,11 @@
 package csu.edu.happygoat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("csu.edu.happygoat.dao")
 public class HappygoatApplication {
 
     public static void main(String[] args) {
