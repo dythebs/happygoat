a47336d9fa4e1f7fe79c3f66f33bcfa1fae3f410
hyfox <404195227@qq.com>
2018-09-05 12:54:16 +0800

    test

--- src/main/java/csu/edu/happygoat/HappygoatApplication.java
+++ src/main/java/csu/edu/happygoat/HappygoatApplication.java
@@ -10,6 +10,5 @@ public class HappygoatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HappygoatApplication.class, args);
-        System.out.println("21");
     }
 }
