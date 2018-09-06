5f5fb6c2817e641f1b7c55d72838d800b78e7a9c
hyfox <404195227@qq.com>
2018-09-06 16:51:14 +0800

    添加redis配置

--- src/main/java/csu/edu/happygoat/HappygoatApplication.java
+++ src/main/java/csu/edu/happygoat/HappygoatApplication.java
@@ -1,8 +1,10 @@
 package csu.edu.happygoat;
 
+import csu.edu.happygoat.util.RedisPool;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 @MapperScan("csu.edu.happygoat.dao")
