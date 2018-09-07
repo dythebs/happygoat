df93017986b634637ba6b553d263a8c3f187c2df
Emma <zrcccrz@163.com>
2018-09-07 14:12:03 +0800

    subcards

--- frontend/src/components/subPageCard/SubCards.jsx
+++ frontend/src/components/subPageCard/SubCards.jsx
@@ -1,54 +1,74 @@
 import React from 'react'
 import "./subCards.css"
 import SubCard from './subCard';
+import {Spin,Icon} from 'antd';
+import Loading from '../loading/Loading';
+
+// const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+const antIcon = <Loading/>
 class SubCards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
 
-      data:this.props.datas
-      //  [
-          // {
-          //   "img": "iq.dxlfile.com/mall/original/2017-09/20170915143021306.jpg-w363h281",
-          //   "tags": [
-          //     "\u6e05\u65b0"
-          //   ],
-          //   "title": "3580\u5143\u7ea7\u522b\u94bb\u77f3\u5316\u5986\u5e08",
-          //   "cast": "\uffe53580",
-          //   "zan": "134",
-          //   "shopname": "\u5b87\u6db5\u9020\u578b\u5b87\u6db5\u89c6\u89c9",
-          //   "span": "\u652f\u6301\u5f02\u5730\u670d\u52a1",
-          //   "shophref": "https://sh.daoxila.com/HunQing/Shop/YuHan-Info",
-          //   "shoppic": "iq.dxlfile.com/mall/avatar/tiny/2017-09/20170914132374100.jpg",
-          //   "anlihref": "/HunQing/Shop/YuHan-Photo-8536",
-          //   "changdi": {
-          //   }
-          // },
-        // ]
+      // data:this.props.datas,
+      ll:false,
+       data:[
+          {
+            "img": "iq.dxlfile.com/mall/original/2017-09/20170915143021306.jpg-w363h281",
+            "tags": [
+              "\u6e05\u65b0"
+            ],
+            "title": "3580\u5143\u7ea7\u522b\u94bb\u77f3\u5316\u5986\u5e08",
+            "cast": "\uffe53580",
+            "zan": "134",
+            "shopname": "\u5b87\u6db5\u9020\u578b\u5b87\u6db5\u89c6\u89c9",
+            "span": "\u652f\u6301\u5f02\u5730\u670d\u52a1",
+            "shophref": "https://sh.daoxila.com/HunQing/Shop/YuHan-Info",
+            "shoppic": "iq.dxlfile.com/mall/avatar/tiny/2017-09/20170914132374100.jpg",
+            "anlihref": "/HunQing/Shop/YuHan-Photo-8536",
+            "changdi": {
+              "location":"sssssssssss",
+              "href":'ssssss'
+            }
+          },
+        ]
     }
   }
 
   componentWillReceiveProps(nextProps){
+    console.log("subCards", this.props, nextProps);
     if(nextProps!==this.props){
-      data:this.props.datas
+     this.setState({
+       data:nextProps.datas,
+       ll:true
+     })
     }
   }
   render() {
-    const {data} = this.state;
+    const {data,ll} = this.state;
     // const item = data[0];
     // console.log(item.title);
-    return (
-      <div className="container">
-        <div className="column">
+    console.log(data);
+    // if(ll){
+      return (
+        <div className="container">
           {
-            data[0]!==undefined &&
-            data.map((item,index) => (
-              <SubCard item={item}/>
+            data.map((item, index) => (
+              <SubCard item={item} key={index}/>
             ))
           }
-        </div>
-      </div >
-    );
+        </div >)
+    // }else{
+    //   return (
+    //     <div className="container">
+    //       <Spin indicator={antIcon} />
+    //       {/* <Loading/> */}
+    //     </div>
+    //   )
+    // }
+
+
   }
 }
 
--- frontend/src/components/subPageCard/subCard.jsx
+++ frontend/src/components/subPageCard/subCard.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import {Icon} from 'antd'
+
+const htttps = "https://";
 class SubCard extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       zan:this.props.item.zan,
-      img:"https://" + this.props.item.img,
+      img:htttps + this.props.item.img,
       title:this.props.item.title,
       changdi:this.props.item.changdi,
-      shoppic: "https://" + this.props.item.shoppic,
+      shoppic: htttps + this.props.item.shoppic,
       span:this.props.item.span,
       shopname:this.props.item.shopname,
-      tags:this.props.item.tags
+      tags:this.props.item.tags,
+      cast:this.props.item.cast
     }
   }
 
   componentWillReceiveProps(nextProps){
+    console.log("subCard",this.props,nextProps);
     if (nextProps!==this.props) {
       this.setState({
         zan: nextProps.item.zan,
@@ -25,55 +29,54 @@ class SubCard extends React.Component {
         shoppic: "https://" + nextProps.item.shoppic,
         span: nextProps.item.span,
         shopname: nextProps.item.shopname,
-        tags: nextProps.item.tags
+        tags: nextProps.item.tags,
+        cast:nextProps.item.cast
       })
     }
   }
    render(){
-    const {zan,img,title,changdi,shoppic,shopname,span,tags} = this.state;
+    const {zan,img,title,changdi,shoppic,shopname,span,tags,cast} = this.state;
     let tagsH,changdiH;
     if(tagsH===[]){
       tagsH = ""
     }else{
       tagsH =
-          <div className="category">
-            {
               tags.map((item, index) => (
-                <div className="category">{item}</div>
+                <div className="category" key={index}>{item}</div>
               ))
-            }
-          </div>
     }
     if(changdi==={}){
       changdiH = ""
     }else{
-      changdiH = <h2 className="sub_title"><a href={changdi.href}>{changdi.location}</a></h2>
+      // changdiH = <h2 className="sub_title location"><a href={changdi.href}>{changdi.location}</a></h2>
+      changdiH = <div className="location"><a href={changdi.href}>{changdi.location}</a></div>
     }
     return(
-      <div className="post-module">
-        <div className="thumbnail">
-          <div className="date">
-            <div className="day">
-              <Icon type="heart" theme="outlined" />
+      <div className="column">
+        <div className="post-module">
+          <div className="thumbnail">
+            <div className="date">
+              <div className="day">
+                <Icon type="heart" theme="outlined" />
+              </div>
+              <div className="month">{zan}</div>
             </div>
-            <div className="month">{zan}</div>
+            <img src={img} />
           </div>
-          <img src={img} />
-        </div>
-        {/* <!-- Post Content--> */}
-        <div className="post-content">
-          {tagsH}
-          <h1 classNameName="title">{title}</h1>
-          {changdiH}
-          <h2 className="sub_title">{ shopname }</h2>
-          <div className="post-meta">
-            {/* <span className="timestamp"><i className="fa fa-clock-">o</i> 6 mins ago</span> */}
-            <span className="timestamp"><img src={shoppic}/></span>
-            {/* <span className="comments"><i className="fa fa-comments"></i><a href="#"> 39 comments</a></span> */}
-            <span className="comments">{span}</span>
-          </div>
-        </div>
+          <div className="post-content">
+            {tagsH}
+            <div className="cast">{cast}</div>
+            <h1 className="title">{title}</h1>
+            <h2 className="sub_title">{shopname}</h2>
+            {changdiH}
+            <div className="post-meta">
+              <span className="timestamp"><img src={shoppic}/></span>
 
+              {/* <span className="comments">{span}</span> */}
+            </div>
+            <div>{span}</div>
+          </div>
+        </div>
       </div>
     );
 }}
