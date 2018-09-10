fcc866caea9489990c926ae630498936bd33bce3
hyfox <404195227@qq.com>
2018-09-10 14:08:30 +0800

    添加图灵机器人接口和部分样例代码

--- src/main/java/csu/edu/happygoat/HappygoatApplication.java
+++ src/main/java/csu/edu/happygoat/HappygoatApplication.java
@@ -1,10 +1,13 @@
 package csu.edu.happygoat;
 
 import csu.edu.happygoat.util.RedisPool;
+import csu.edu.happygoat.util.RobotUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import redis.clients.jedis.Jedis;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @MapperScan("csu.edu.happygoat.dao")
@@ -12,5 +15,15 @@ public class HappygoatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HappygoatApplication.class, args);
+        /*
+        Jedis jedis = RedisPool.getJedis();
+        RobotUtil robotUtil = new RobotUtil();
+        try {
+            System.out.println(robotUtil.getReplyFromRobot("你们有些什么服务"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(jedis.get("test444"));
+        */
     }
 }
