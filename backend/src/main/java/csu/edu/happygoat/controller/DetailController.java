6a5e14406641bbdd3f53ce46421e1c16f0571327
dythebs <244271172@qq.com>
2018-09-19 00:49:02 +0800

    婚礼策划详情

--- backend/src/main/java/csu/edu/happygoat/controller/DetailController.java
+++ backend/src/main/java/csu/edu/happygoat/controller/DetailController.java
@@ -1,5 +1,7 @@
 package csu.edu.happygoat.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import csu.edu.happygoat.service.DetailService;
 import csu.edu.happygoat.util.UrlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.rmi.CORBA.Util;
 
 @RestController
 @CrossOrigin
@@ -18,6 +22,8 @@ public class DetailController {
     @GetMapping("/detail/hunlicehua/{url}")
     public String detailHunlicehua(@PathVariable("url") String url) {
         url = UrlUtil.trimUrl(url);
-        return detailService.getHunlicehuaData(url);
+        String content = detailService.getHunlicehuaData(url);
+        JSONObject jsonObject = JSONObject.parseObject(content);
+        return jsonObject.getString("content");
     }
 }
