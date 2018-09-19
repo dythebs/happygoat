a303bf29d45de22b94e6330fe955cad9f75c88cf
jqlucky <43259658+jqlucky@users.noreply.github.com>
2018-09-19 20:21:31 +0800

    修改联系我们

--- frontend/src/components/footer/Footer_.jsx
+++ frontend/src/components/footer/Footer_.jsx
@@ -3,11 +3,23 @@ import "./footer_.css"
 class Footer_ extends React.Component {
    render(){
     return(
+      <footer_ className={scrollToBottom?"topper":""}>
       <div className="footer-area">
-      <ul>
-            <li>联系我们吧</li>
-          </ul>
+      <P>省钱省心·自由选择</P>
       </div>
+      <div className="footer-area2">
+      <p>专业服务·贴心管家</p>
+      </div>
+      <div className="footer-">
+        <p>关于我们</p>
+        <br/>
+        <br/>
+        <p>加入我们</p>
+        <br/>
+        <br/>
+        <p>联系我们</p>
+      </div>
+      </footer_>
     );
 }}
 
