71cf68c81f66d956529b2aa6021edbca65d4c97e
jqlucky <43259658+jqlucky@users.noreply.github.com>
2018-09-19 18:43:57 +0800

    联系我们

--- frontend/src/components/footer/Footer_.jsx
+++ frontend/src/components/footer/Footer_.jsx
@@ -4,7 +4,9 @@ class Footer_ extends React.Component {
    render(){
     return(
       <div className="footer-area">
-
+      <ul>
+            <li>联系我们吧</li>
+          </ul>
       </div>
     );
 }}
